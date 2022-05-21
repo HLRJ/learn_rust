71e5a39d778e2faab0883c3f3c525c009c4b6678
HLRJ <befun@vip.qq.com>
2022-05-21 08:55:57 +0800

    枚举是一个不同于结构体的定义自定义数据类型的方式。

---
+++ src/06enum/enum_ip.rs
@@ -0,0 +1,21 @@
+
+//里面每一个的项称为该枚举类型的一个变体
+
+
+fn main() {
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            // 在这里定义方法体
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
