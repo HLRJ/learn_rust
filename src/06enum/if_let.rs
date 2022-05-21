0bcbb83ad4c037ae4d59e60e8c6d8185f73619e6
HLRJ <befun@vip.qq.com>
2022-05-22 01:11:35 +0800

    match的简洁模式  if let  else

---
+++ src/06enum/if_let.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let coin = Coin::Penny;
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+}
+
+//if let  与下面的match等价
